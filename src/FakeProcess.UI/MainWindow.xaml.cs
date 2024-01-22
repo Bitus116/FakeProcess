48446332135289ff4b997694f7dc2ff3b94797ee
Хамидуллин Тимур Денисович <hamidullin_td@prog.zarnitza.local>
2024-01-22 18:33:20 +0300

    ui

---
+++ src/FakeProcess.UI/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using FakeProcess.UI.ViewModel;
+using System.Windows;
+
+namespace FakeProcess.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ src/FakeProcess.UI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeProcess.UI.ViewModel
+{
+    public class MainWindowViewModel
+    {
+    }
+}
